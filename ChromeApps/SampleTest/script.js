ef70b4ded9404167431b443938d0c4332f845d58
Yourhomie164 <yourhomie164@gmail.com>
2023-04-23 01:10:22 -0400

    Added Flyout Menu

--- ChromeApps/SampleTest/script.js
+++ ChromeApps/SampleTest/script.js
@@ -1,1 +1,9 @@
-// HELLO WORLD
+function displayMenu() {
+   document.getElementById('menuFlyout').style.display = "block";
+   document.getElementById('menuBackground').style.display = "block";
+}
+
+function hideMenu() {
+   document.getElementById('menuFlyout').style.display = "none";
+   document.getElementById('menuBackground').style.display = "none";
+}
